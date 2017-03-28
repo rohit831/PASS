5c89bd45053bd17f10d44b3744ec8d95af299409
Gaurav <gauravw0407@gmail.com>
2017-03-28 17:12:22 +0530

    Boutique App for Purchasing Items

---
+++ app/src/main/java/com/gr/pass/HomeScreen.java
@@ -0,0 +1,80 @@
+package com.gr.pass;
+
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class HomeScreen extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_home) {
+            Toast.makeText(this, "Home clicked!!", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.nav_wishlist) {
+            Toast.makeText(this, "Wishlist clicked!!", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.nav_sarees) {
+            Toast.makeText(this, "Sarees clicked!!", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.nav_suits) {
+
+            Toast.makeText(this, "Suits clicked!!", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.nav_leggings) {
+            Toast.makeText(this, "leggings clicked!!", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.nav_about_us) {
+            Toast.makeText(this, "About Us clicked!!", Toast.LENGTH_SHORT).show();
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
